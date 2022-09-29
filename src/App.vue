ba38de00ddcddb9f143cd05c56c6cf777088aa63
skeleton567 <mikkelletoo@gmail.com>
2022-09-29 16:35:20 +0400

    feat: add first page

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
-    <h1 class="text-3xl text-red-300 font-bold underline">
-    Hello world!
-  </h1>
+  <base-wrap>
+    <router-view></router-view>
+  </base-wrap>
 </template>
 
 <script>
-  import './assets/tailwind.css'
+import "./assets/tailwind.css";
+import BaseWrap from "./components/BaseWrap.vue";
+
+export default {
+  components: { BaseWrap },
+};
 </script>
-  
+
+<style>
+@font-face {
+  font-family: "BPGArial";
+  src: local("BPGArial"),
+    url(.assets/fonts/bpg_arial_2009.ttf) format("truetype");
+}
+</style>
---
+++ src/pages/StartPage.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="h-full w-full flex flex-col justify-center items-center">
+    <transition name="bird" @after-enter="afterImgEnter" appear>
+      <img class="w-24 h-24" src="../assets/images/StartBird.png" alt="" />
+    </transition>
+    <div v-if="imageSet" class="w-96 h-24 bg-[#B5C0C7] z-50"></div>
+    <transition name="link" appear>
+      <div
+        v-if="imageSet"
+        class="font-bold text-3xl w-24 flex justify-center z-0"
+      >
+        <router-link to="/" class="text-center h"
+          >კითხვარის დაწყება</router-link
+        >
+      </div>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      imageSet: false,
+    };
+  },
+  methods: {
+    afterImgEnter() {
+      this.imageSet = true;
+    },
+  },
+};
+</script>
+
+<style>
+.bird-enter-from {
+  scale: 25;
+}
+.bird-enter-active {
+  transition: all 0.5s ease-out;
+}
+.bird-enter-to {
+  translate: 0px -86px;
+  scale: 1;
+}
+
+.link-enter-from {
+  translate: 0px -100px;
+  opacity: 0;
+}
+.link-enter-active {
+  transition: all 1s ease-out;
+}
+.link-enter-to {
+  translate: 0px 0px;
+  opacity: 1;
+}
+
+.h:hover {
+  text-shadow: 2px 0px 0 #b5c0c7, 3px 0px 0 black;
+}
+</style>
