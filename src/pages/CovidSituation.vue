9f642d44a2e6995757c639267da62dd77c0654c7
skeleton567 <mikkelletoo@gmail.com>
2022-10-03 11:21:17 +0400

    feat: add second page validation

--- src/pages/CovidSituation.vue
+++ src/pages/CovidSituation.vue
@@ -1,157 +1,134 @@
 <template>
-    <Form>
-  <the-header pageNum="2">
-    <div class="flex justify-between">
-      <section class="mt-10 w-[500px]">
-        <div>
-          <h2 class="font-bold text-xl">გაქვს გადატანილი Covid-19?*</h2>
-          <div class="mt-2 mb-5 ml-5">
-            <Field
-              v-slot="{ covidStatus }"
-              name="covidStatus"
-              type="radio"
-              :value="yes"
-            >
+  <Form v-slot="{ meta, values }" :validation-schema="schema">
+    <the-header pageNum="2">
+      <div class="flex justify-between">
+        <section class="mt-10 w-[500px]">
+          <div>
+            <h2 class="font-bold text-xl">გაქვს გადატანილი Covid-19?*</h2>
+            <div class="mt-2 mb-5 ml-5">
               <label class="text-lg ml-2">
-                <input
+                <Field
                   type="radio"
                   class="appearance-none rounded-full bg-inherit box-shadow border-2 border-[#B5C0C7] p-1 checked:bg-neutral-900"
                   name="covidStatus"
-                  v-bind="covidStatus"
-                  :value="yes"
+                  :value="'yes'"
                 />
                 კი
               </label>
-            </Field>
-          </div>
-          <div class="ml-5 mb-5">
-            <Field
-              v-slot="{ covidStatus }"
-              name="covidStatus"
-              type="radio"
-              :value="no"
-            >
+            </div>
+            <div class="ml-5 mb-5">
               <label class="text-lg ml-2">
-                <input
+                <Field
                   type="radio"
                   class="appearance-none rounded-full bg-inherit box-shadow border-2 border-[#B5C0C7] p-1 checked:bg-neutral-900"
                   name="covidStatus"
-                  v-bind="covidStatus"
-                  :value="no"
+                  :value="'no'"
                 />
                 არა
               </label>
-            </Field>
-          </div>
-          <div class="ml-5">
-            <Field
-              v-slot="{ covidStatus }"
-              name="covidStatus"
-              type="radio"
-              :value="now"
-            >
+            </div>
+            <div class="ml-5">
               <label class="text-lg ml-2">
-                <input
+                <Field
                   type="radio"
                   class="appearance-none rounded-full bg-inherit box-shadow border-2 border-[#B5C0C7] p-1 checked:bg-neutral-900"
                   name="covidStatus"
-                  v-bind="covidStatus"
-                  :value="now"
+                  :value="'now'"
                 />
                 ახლა მაქვს
               </label>
-            </Field>
+            </div>
           </div>
-        </div>
-        <div class="mt-10">
-          <h2 class="font-bold text-xl">
-            ანტისხეულების ტესტი გაქვს გაკეთებული?*
-          </h2>
-          <div class="mt-2 mb-5 ml-5">
-            <Field
-              v-slot="{ covidTest}"
-              name="covidTest"
-              type="radio"
-              :value="testYes"
-            >
+          <div v-if="values.covidStatus === 'yes'" class="mt-10">
+            <div class="ml-5">
+              <h2 class="font-bold text-xl">
+                ანტისხეულების ტესტი გაქვს გაკეთებული?*
+              </h2>
               <label class="text-lg ml-2">
-                <input
+                <Field
                   type="radio"
                   class="appearance-none rounded-full bg-inherit box-shadow border-2 border-[#B5C0C7] p-1 checked:bg-neutral-900"
                   name="covidTest"
-                  v-bind="covidTest"
-                  :value="testYes"
+                  :value="'testYes'"
                 />
                 კი
               </label>
-            </Field>
-          </div>
-          <div class="mt-2 mb-5 ml-5">
-            <Field
-              v-slot="{ covidTest}"
-              name="covidTest"
-              type="radio"
-              :value="testNo"
-            >
+            </div>
+            <div class="mt-2 mb-5 ml-5">
               <label class="text-lg ml-2">
-                <input
+                <Field
                   type="radio"
                   class="appearance-none rounded-full bg-inherit box-shadow border-2 border-[#B5C0C7] p-1 checked:bg-neutral-900"
                   name="covidTest"
-                  v-bind="covidTest"
-                  :value="testNo"
+                  :value="'testNo'"
                 />
                 არა
               </label>
-            </Field>
+            </div>
           </div>
-        </div>
 
-        <div>
-          <h2 class="font-bold text-xl">
-            თუ გახსოვს, გთხოვ მიუთითე ტესტის მიახლოებითი რიცხვი და ანტისხეულების
-            რაოდენობა*
-          </h2>
-          <Field
-            class="outline-none ml-5 w-full px-5 py-2 bg-inherit border border-gray-600 mt-3 placeholder-gray-500"
-            type="number"
-            name="date"
-            placeholder="რიცხვი"
-          />
-          <Field
-            class="outline-none ml-5 w-full px-5 py-2 bg-inherit border border-gray-600 mt-3 placeholder-gray-500"
-            name="antiBodies"
-            placeholder="ანტისხეულების რაოდენობა"
-          />
-        </div>
-        <div>
-          <h2 class="font-bold text-xl">
-            მიუთითე მიახლოებითი პერიოდი (დღე/თვე/წელი) როდის გქონდა Covid-19*
-          </h2>
-          <Field
-            class="outline-none ml-5 w-full px-5 py-2 bg-inherit border border-gray-600 mt-3 placeholder-gray-500"
-            type="number"
-            name="period"
-            placeholder="დდ/თთ/წწ"
-          />
-        </div>
-      </section>
-      <img
-        class="h-[600px]"
-        src="@/assets/images/temperature.png"
-        alt="Boy with temperature"
-      />
-    </div>
-  </the-header>
-  <footer class="mt-20">
-    <the-footer
-      pageNum="2"
-      :isActive="true"
-      :previousPage="{ name: 'information' }"
-      :nextPage="{ name: 'covidSituation' }"
-    ></the-footer>
-  </footer>
-</Form>
+          <div
+            v-if="
+              values.covidStatus === 'yes' && values.covidTest === 'testYes'
+            "
+          >
+            <h2 class="font-bold text-xl">
+              თუ გახსოვს, გთხოვ მიუთითე ტესტის მიახლოებითი რიცხვი და
+              ანტისხეულების რაოდენობა*
+            </h2>
+            <Field
+              class="outline-none ml-5 w-full px-5 py-2 bg-inherit border border-gray-600 mt-3 placeholder-gray-500"
+              type="number"
+              name="date"
+              placeholder="რიცხვი"
+            />
+            <Field
+              class="outline-none ml-5 w-full px-5 py-2 bg-inherit border border-gray-600 mt-3 placeholder-gray-500"
+              name="antiBodies"
+              placeholder="ანტისხეულების რაოდენობა"
+            />
+          </div>
+
+          <div
+            v-if="values.covidStatus === 'yes' && values.covidTest === 'testNo'"
+          >
+            <label for="period" class="font-bold text-xl">
+              მიუთითე მიახლოებითი პერიოდი (დღე/თვე/წელი) როდის გქონდა Covid-19*
+            </label>
+            <Field
+              class="outline-none ml-5 w-full px-5 py-2 bg-inherit border border-gray-600 mt-3 placeholder-gray-500"
+              id="period"
+              type="text"
+              name="period"
+              placeholder="დდ/თთ/წწ"
+            />
+            <ErrorMessage
+              class="text-red-400 text-sm px-5 ml-4"
+              name="period"
+            />
+          </div>
+        </section>
+        <img
+          class="h-[600px]"
+          src="@/assets/images/temperature.png"
+          alt="Boy with temperature"
+        />
+      </div>
+    </the-header>
+    <footer class="mt-20">
+      <the-footer
+        pageNum="2"
+        :isActive="
+          meta.valid ||
+          values.covidStatus === 'no' ||
+          values.covidStatus === 'now' ||
+          values.covidTest === 'testYes'
+        "
+        :previousPage="{ name: 'information' }"
+        :nextPage="{ name: 'covidSituation' }"
+      ></the-footer>
+    </footer>
+  </Form>
 </template>
 
 <script>
@@ -161,6 +138,34 @@ export default {
     Field,
     ErrorMessage,
     Form,
+  },
+  data() {
+    return {
+      schema: {
+        covidStatus: (value) => {
+          if (value) {
+            return true;
+          }
+          return false;
+        },
+        covidTest: (value) => {
+          if (value) {
+            return true;
+          }
+          return false;
+        },
+        period: (value) => {
+          const dateRegex =
+            /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+          if (dateRegex.test(value)) {
+            return true;
+          } else if (!value) {
+            return "";
+          }
+          return "რიცხი უნდა ჩაიწეროს დღე/თვე/წელი ფორმატით";
+        },
+      },
+    };
   },
 };
 </script>
